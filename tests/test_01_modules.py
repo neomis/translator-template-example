b4db1c103903a68163b0008a38d9e38bb83a1178
Justin Blair <neomis@gmail.com>
2022-07-21 21:29:55 -0700

    Added test file

--- tests/test_01_modules.py
+++ tests/test_01_modules.py
@@ -1,9 +1,10 @@
 """Run module tests."""
 import pytest
 
+
 @pytest.mark.test_modules
 def test_main():
     """Test main package works."""
     from translator_template.main import spool_file  # pylint: disable=import-outside-toplevel, unused-import
-    spool_file('tests/spooler/queued/file1.example')
+    spool_file('tests/file1.example')
     assert True
